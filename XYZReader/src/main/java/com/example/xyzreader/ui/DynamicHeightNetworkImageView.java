fe80becd6aa04cd5dc9e326a75437f11911f73b5
AhmedMoneeb <ah.moneeb@gmail.com>
2017-07-28 04:28:02 +0200

    Initial commit

---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/DynamicHeightNetworkImageView.java
@@ -0,0 +1,42 @@
+package com.example.xyzreader.ui;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.icu.util.Measure;
+import android.util.AttributeSet;
+
+import com.android.volley.toolbox.NetworkImageView;
+
+public class DynamicHeightNetworkImageView extends NetworkImageView {
+    private float mAspectRatio = 1.5f;
+
+    public DynamicHeightNetworkImageView(Context context) {
+        super(context);
+    }
+
+    public DynamicHeightNetworkImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DynamicHeightNetworkImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public void setAspectRatio(float aspectRatio) {
+        mAspectRatio = aspectRatio;
+        requestLayout();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        Drawable drawable = getDrawable();
+
+        if(drawable!=null){
+            int width = MeasureSpec.getSize(widthMeasureSpec);
+            int height = (int) Math.ceil((float) width * (float) drawable.getIntrinsicHeight() / (float) drawable.getIntrinsicWidth());
+            setMeasuredDimension(width, height);
+        }else{
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        }
+    }
+}
